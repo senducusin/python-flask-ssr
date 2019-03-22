404f060e92da94adc74127c4d00a8b049a113a28
Jansen <senducusin@gmail.com>
2019-03-22 10:05:18 +0800

    added templates and routes

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from data import Articles
+
+app = Flask(__name__)
+app.debug = True
+
+Articles = Articles()
+
+# Home route
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+# Route for about
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Route for list of articles
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles=Articles)
+
+# Route for individual articles
+@app.route('/articles/<string:id>')
+def article(id):
+    return render_template('article.html', id=id)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': "Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.",
+            'author': 'Sen',
+            'created_date': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': "Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.",
+            'author': 'SenC',
+            'created_date': '04-26-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': "Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.",
+            'author': 'SenCD',
+            'created_date': '04-26-2017'
+        }
+    ]
+    return articles
